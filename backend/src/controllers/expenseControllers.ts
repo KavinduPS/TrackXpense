505cc8a306edcf739acedf0ad516c5010401f318
Kavindu Wedagedara <ksumanathilake@gmail.com>
2024-08-26 09:30:43 +0530

    created expenses CRUD operations

--- backend/src/controllers/expenseControllers.ts
+++ backend/src/controllers/expenseControllers.ts
@@ -13,18 +13,55 @@ const getExpenses = async (req: Request, res: Response) => {
 };
 
 //Create expense - api/expenses
-const createExpense = (req: Request, res: Response) => {
-  res.json({ message: "Set Expense" });
+const createExpense = async (req: Request, res: Response) => {
+  try {
+    console.log(req.body);
+    if (
+      !req.body.title ||
+      !req.body.amount ||
+      !req.body.date ||
+      !req.body.category
+    ) {
+      return res.status(400).json({ message: "Please enter required fields" });
+    }
+    const newExpense = await Expense.create({
+      title: req.body.title,
+      amount: req.body.amount,
+      date: req.body.date,
+      category: req.body.category,
+      description: req.body.description,
+    });
+    res.status(201).json(newExpense);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: "Error creating expense" });
+  }
 };
 
 //Edit expense - api/expenses/:id
-const editExpense = (req: Request, res: Response) => {
-  res.json({ message: `Edit Expense ${req.params.id}` });
+const editExpense = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const expense = await Expense.findById(id);
+  if (!expense) {
+    return res.status(400).json({ message: "Expense not found" });
+  }
+  const updatedExpense = await Expense.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.status(200).json(updatedExpense);
 };
 
 //Delete expense - api/expenses/:id
-const deleteExpense = (req: Request, res: Response) => {
-  res.json({ message: `Delete Expense ${req.params.id} ` });
+const deleteExpense = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const expense = await Expense.findById(id);
+  if (!expense) {
+    return res.status(400).json({ message: "Expense not found" });
+  }
+  const deletedExpense = await Expense.findByIdAndDelete(id);
+  res.status(200).json(deletedExpense?._id);
 };
 
 export { getExpenses, createExpense, editExpense, deleteExpense };
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/expenses", expensesRoutes);
 
