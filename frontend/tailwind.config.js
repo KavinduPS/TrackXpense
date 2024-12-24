ab508c627645dbcfd4aaa732abaa1154d608e710
Lakshika <lakshikagunasekara765@gmail.com>
2024-12-25 02:31:36 +0530

    Login page changes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,8 +3,18 @@ module.exports = {
   content: ["./src/**/*.{tsx,ts,js,jsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
       backgroundImage: {
         "signup-bg": "url('/src/assets/x_logo.png')",
+        "pg-image": "url('/src/assets/pg.png')",
+        "login-image": "url('/src/assets/login-image.jpg')",
+        "piggy-img": "url('/src/assets/pg.jpg')",
+      },
+      backgroundSize: {
+        50: "50%",
+        75: "75%",
       },
       colors: {
         Dark: "#1F1F22", //#1B1B1E
